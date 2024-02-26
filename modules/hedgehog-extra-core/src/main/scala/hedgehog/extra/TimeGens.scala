7dde2117cd108e02d8c76dc155aaa2bb82c19e1c
Kevin Lee <kevin.code@kevinlee.io>
2024-02-26 22:01:20 +1100

    Close #97 - [hedgehog-extra-core] Add TimeGens.genInstantFrom to generate java.time.Instant with duration range

--- modules/hedgehog-extra-core/src/main/scala/hedgehog/extra/TimeGens.scala
+++ modules/hedgehog-extra-core/src/main/scala/hedgehog/extra/TimeGens.scala
@@ -3,6 +3,7 @@ package hedgehog.extra
 import hedgehog._
 
 import java.time.Instant
+import scala.concurrent.duration.FiniteDuration
 
 /** @author Kevin Lee
   * @since 2024-02-21
@@ -13,4 +14,21 @@ object TimeGens {
     val toMillis   = to.toEpochMilli
     Gen.long(Range.linear(fromMillis, toMillis)).map(Instant.ofEpochMilli)
   }
+
+  def genInstantFrom(baseInstant: Instant, durationAgo: FiniteDuration, durationAfter: FiniteDuration): Gen[Instant] = {
+    val from        = baseInstant.minusMillis(durationAgo.toMillis)
+    val fromSeconds = from.getEpochSecond
+    val fromNanos   = from.getNano
+
+    val to        = baseInstant.plusMillis(durationAfter.toMillis)
+    val toSeconds = to.getEpochSecond
+    val toNanos   = to.getNano
+
+    Gen
+      .long(Range.linear(fromSeconds * 1000000000 + fromNanos, toSeconds * 1000000000 + toNanos))
+      .map(l => Instant.ofEpochSecond(l / 1000000000L, l % 1000000000))
+//      .long(Range.linear(fromSeconds * 1_000_000_000 + fromNanos, toSeconds * 1_000_000_000 + toNanos))
+//      .map(l => Instant.ofEpochSecond(l / 1_000_000_000L, l % 1_000_000_000))
+  }
+
 }
--- modules/hedgehog-extra-core/src/test/scala/hedgehog/extra/TimeGensSpec.scala
+++ modules/hedgehog-extra-core/src/test/scala/hedgehog/extra/TimeGensSpec.scala
@@ -12,7 +12,8 @@ import java.time.temporal.ChronoUnit
 object TimeGensSpec extends Properties {
 
   override def tests: List[Test] = List(
-    property("test TimeGens.genInstant(from, to)", testGenInstant)
+    property("test TimeGens.genInstant(from, to)", testGenInstant),
+    property("test TimeGens.genInstantFrom(from, to)", testGenInstantFrom)
   )
 
   def testGenInstant: Property =
@@ -32,4 +33,76 @@ object TimeGensSpec extends Properties {
            |    to=${to.toString}
            |""".stripMargin
       )
+
+  def testGenInstantFrom: Property = {
+    import scala.concurrent.duration._
+
+    for {
+      base   <- Gen.constant(Instant.now()).log("base")
+      from   <- Gen.int(Range.linear(0, 100)).map(_.days).log("from")
+      to     <- Gen.int(Range.linear(0, 100)).map(_.days).log("to")
+      actual <- TimeGens.genInstantFrom(base, from, to).log("actual")
+    } yield {
+      val fromExpected = base.minusMillis(from.toMillis)
+      val toExpected   = base.plusMillis(to.toMillis)
+      println(
+        s"""        base=${base.toString}
+           |             ${base.toEpochMilli} ms
+           |             ${base.getEpochSecond} s
+           |             ${base.getNano} ns
+           |        from=${from.toString}
+           |          to=${to.toString}
+           |      actual=${actual.toString}
+           |fromExpected=${fromExpected.toString}
+           |  toExpected=${toExpected.toString}
+           |--------------------------------------
+           |""".stripMargin
+      )
+      Result.all(
+        List(
+          Result.any(
+            List(
+              (actual ==== fromExpected).log(
+                s"""actual ==== fromExpected failed
+                 |      actual=${actual.toString}
+                 |fromExpected=${fromExpected.toString}
+                 |  toExpected=${toExpected.toString}
+                 |""".stripMargin
+              ),
+              Result
+                .assert(actual.isAfter(fromExpected))
+                .log(
+                  s"""actual.isAfter(fromExpected) failed
+                   |      actual=${actual.toString}
+                   |fromExpected=${fromExpected.toString}
+                   |  toExpected=${toExpected.toString}
+                   |""".stripMargin
+                )
+            )
+          ),
+          Result.any(
+            List(
+              (actual ==== toExpected).log(
+                s"""actual ==== toExpected failed
+               |      actual=${actual.toString}
+               |fromExpected=${fromExpected.toString}
+               |  toExpected=${toExpected.toString}
+               |""".stripMargin
+              ),
+              Result
+                .assert(actual.isBefore(toExpected))
+                .log(
+                  s"""actual.isBefore(toExpected) failed
+               |      actual=${actual.toString}
+               |fromExpected=${fromExpected.toString}
+               |  toExpected=${toExpected.toString}
+               |""".stripMargin
+                )
+            )
+          )
+        )
+      )
+    }
+  }
+
 }
